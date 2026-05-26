3aed0fda61686736c12d7780d84bdf6aca6f25c7
Hsuminhui <0802mandy@gmail.com>
2026-05-26 17:26:53 +0800

    加入專案檔案。

---
+++ Othello/Othello.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Othello
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pnlBoard = new System.Windows.Forms.Panel();
+            this.lblBlackScore = new System.Windows.Forms.Label();
+            this.lblWhiteScore = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnUndo = new System.Windows.Forms.Button();
+            this.mode = new System.Windows.Forms.ComboBox();
+            this.timerAI = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.遊戲說明ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.關於ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+
+            // pnlBoard
+            this.pnlBoard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlBoard.Location = new System.Drawing.Point(20, 90);
+            this.pnlBoard.Name = "pnlBoard";
+            this.pnlBoard.Size = new System.Drawing.Size(480, 480);
+            this.pnlBoard.TabIndex = 0;
+            this.pnlBoard.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlBoard_Paint);
+            this.pnlBoard.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pnlBoard_MouseClick);
+
+            // lblBlackScore
+            this.lblBlackScore.AutoSize = true;
+            this.lblBlackScore.Font = new System.Drawing.Font("標楷體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblBlackScore.Location = new System.Drawing.Point(520, 130);
+            this.lblBlackScore.Name = "lblBlackScore";
+            this.lblBlackScore.Size = new System.Drawing.Size(85, 28);
+            this.lblBlackScore.TabIndex = 1;
+            this.lblBlackScore.Text = "黑棋:";
+ 
+            // lblWhiteScore
+            this.lblWhiteScore.AutoSize = true;
+            this.lblWhiteScore.Font = new System.Drawing.Font("標楷體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblWhiteScore.Location = new System.Drawing.Point(520, 190);
+            this.lblWhiteScore.Name = "lblWhiteScore";
+            this.lblWhiteScore.Size = new System.Drawing.Size(85, 28);
+            this.lblWhiteScore.TabIndex = 2;
+            this.lblWhiteScore.Text = "白棋:";
+
+            // lblStatus
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("標楷體", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lblStatus.Location = new System.Drawing.Point(20, 610);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(143, 28);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "輪到: 黑棋";
+
+            // btnNewGame
+            this.btnNewGame.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNewGame.Location = new System.Drawing.Point(520, 480);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(120, 40);
+            this.btnNewGame.TabIndex = 4;
+            this.btnNewGame.Text = "新遊戲";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+
+            // btnUndo
+            this.btnUndo.Font = new System.Drawing.Font("標楷體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnUndo.Location = new System.Drawing.Point(520, 535);
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(120, 40);
+            this.btnUndo.TabIndex = 5;
+            this.btnUndo.Text = "悔棋";
+            this.btnUndo.UseVisualStyleBackColor = true;
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+
+            // mode
+            this.mode.FormattingEnabled = true;
+            this.mode.Items.AddRange(new object[] {
+            "人 vs 人",
+            "人 vs AI (簡易)",
+            "人 vs AI (稍難)"});
+            this.mode.Location = new System.Drawing.Point(20, 45);
+            this.mode.Name = "mode";
+            this.mode.Size = new System.Drawing.Size(180, 26);
+            this.mode.TabIndex = 6;
+
+            // timerAI
+            this.timerAI.Interval = 500;
+
+            // menuStrip1
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.遊戲說明ToolStripMenuItem,
+            this.關於ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(720, 32);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+
+            // 遊戲說明ToolStripMenuItem
+            this.遊戲說明ToolStripMenuItem.Name = "遊戲說明ToolStripMenuItem";
+            this.遊戲說明ToolStripMenuItem.Size = new System.Drawing.Size(134, 28);
+            this.遊戲說明ToolStripMenuItem.Text = "遊戲規則說明";
+            this.遊戲說明ToolStripMenuItem.Click += new System.EventHandler(this.遊戲說明ToolStripMenuItem_Click);
+
+            // 關於ToolStripMenuItem
+            this.關於ToolStripMenuItem.Name = "關於ToolStripMenuItem";
+            this.關於ToolStripMenuItem.Size = new System.Drawing.Size(62, 28);
+            this.關於ToolStripMenuItem.Text = "關於";
+            this.關於ToolStripMenuItem.Click += new System.EventHandler(this.關於ToolStripMenuItem_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 680);
+            this.Controls.Add(this.mode);
+            this.Controls.Add(this.btnUndo);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblWhiteScore);
+            this.Controls.Add(this.lblBlackScore);
+            this.Controls.Add(this.pnlBoard);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "黑白棋";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBoard;
+        private System.Windows.Forms.Label lblBlackScore;
+        private System.Windows.Forms.Label lblWhiteScore;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnUndo;
+        private System.Windows.Forms.ComboBox mode;
+        private System.Windows.Forms.Timer timerAI;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 遊戲說明ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 關於ToolStripMenuItem;
+    }
+}
---
+++ Othello/Othello.cs
@@ -0,0 +1,688 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Othello
+{
+    public partial class Form1 : Form
+    {
+        private const int BoardSize = 8;
+        private int cellSize;
+
+        // 0 = 空格，1 = 黑棋，2 = 白棋
+        private int[,] board = new int[BoardSize, BoardSize];
+
+        // 黑棋先手
+        private int currentPlayer = 1;
+
+        private bool gameOver = false;
+
+        private readonly int[] dr = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        private readonly int[] dc = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+        private readonly Random random = new Random();
+
+        private class GameState
+        {
+            public int[,] BoardSnap { get; set; }
+            public int Player { get; set; }
+        }
+
+        private Stack<GameState> history = new Stack<GameState>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pnlBoard.BackColor = Color.FromArgb(34, 139, 70);
+
+            mode.DropDownStyle = ComboBoxStyle.DropDownList;
+            if (mode.Items.Count > 0)
+            {
+                mode.SelectedIndex = 0;
+            }
+
+            timerAI.Tick += timerAI_Tick;
+            mode.SelectedIndexChanged += mode_SelectedIndexChanged;
+
+            InitGame();
+        }
+
+        private void InitGame()
+        {
+            timerAI.Stop();
+
+            board = new int[BoardSize, BoardSize];
+            currentPlayer = 1;
+            gameOver = false;
+            history.Clear();
+
+            int mid = BoardSize / 2;
+
+            board[mid - 1, mid - 1] = 2;
+            board[mid - 1, mid] = 1;
+            board[mid, mid - 1] = 1;
+            board[mid, mid] = 2;
+
+            UpdateUI();
+            pnlBoard.Invalidate();
+        }
+
+        private int GetCellSize()
+        {
+            int size = Math.Min(pnlBoard.Width, pnlBoard.Height) / BoardSize;
+            if (size <= 0)
+            {
+                size = 1;
+            }
+
+            return size;
+        }
+
+        private void pnlBoard_Paint(object sender, PaintEventArgs e)
+        {
+            cellSize = GetCellSize();
+
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            int boardPixelSize = BoardSize * cellSize;
+
+            using (SolidBrush boardBrush = new SolidBrush(Color.FromArgb(34, 139, 70)))
+            {
+                g.FillRectangle(boardBrush, 0, 0, boardPixelSize, boardPixelSize);
+            }
+
+            using (Pen gridPen = new Pen(Color.FromArgb(0, 80, 40), 1))
+            {
+                for (int i = 0; i <= BoardSize; i++)
+                {
+                    g.DrawLine(gridPen, i * cellSize, 0, i * cellSize, boardPixelSize);
+                    g.DrawLine(gridPen, 0, i * cellSize, boardPixelSize, i * cellSize);
+                }
+            }
+
+            DrawStarPoints(g);
+            DrawPiecesAndHints(g);
+        }
+
+        private void DrawStarPoints(Graphics g)
+        {
+            int[] starCells = { 2, 5 };
+
+            foreach (int r in starCells)
+            {
+                foreach (int c in starCells)
+                {
+                    int x = c * cellSize + cellSize / 2 - 4;
+                    int y = r * cellSize + cellSize / 2 - 4;
+
+                    g.FillEllipse(Brushes.DarkGreen, x, y, 8, 8);
+                }
+            }
+        }
+
+        private void DrawPiecesAndHints(Graphics g)
+        {
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    int x = col * cellSize + 4;
+                    int y = row * cellSize + 4;
+                    int size = cellSize - 8;
+
+                    if (size <= 0)
+                    {
+                        continue;
+                    }
+
+                    if (board[row, col] == 1)
+                    {
+                        DrawBlackPiece(g, x, y, size);
+                    }
+                    else if (board[row, col] == 2)
+                    {
+                        DrawWhitePiece(g, x, y, size);
+                    }
+                    else if (!gameOver && !IsAITurn() && IsValidMove(row, col, currentPlayer))
+                    {
+                        DrawHint(g, x, y, size);
+                    }
+                }
+            }
+        }
+
+        private void DrawBlackPiece(Graphics g, int x, int y, int size)
+        {
+            using (SolidBrush shadow = new SolidBrush(Color.FromArgb(90, 0, 0, 0)))
+            {
+                g.FillEllipse(shadow, x + 2, y + 2, size, size);
+            }
+
+            g.FillEllipse(Brushes.Black, x, y, size, size);
+
+            using (SolidBrush highlight = new SolidBrush(Color.FromArgb(60, 255, 255, 255)))
+            {
+                g.FillEllipse(highlight, x + size / 5, y + size / 8, size / 3, size / 4);
+            }
+        }
+
+        private void DrawWhitePiece(Graphics g, int x, int y, int size)
+        {
+            using (SolidBrush shadow = new SolidBrush(Color.FromArgb(100, 100, 100)))
+            {
+                g.FillEllipse(shadow, x + 2, y + 2, size, size);
+            }
+
+            g.FillEllipse(Brushes.White, x, y, size, size);
+
+            using (Pen outline = new Pen(Color.LightGray, 1))
+            {
+                g.DrawEllipse(outline, x, y, size, size);
+            }
+
+            using (SolidBrush highlight = new SolidBrush(Color.FromArgb(70, 255, 255, 255)))
+            {
+                g.FillEllipse(highlight, x + size / 5, y + size / 8, size / 3, size / 4);
+            }
+        }
+
+        private void DrawHint(Graphics g, int x, int y, int size)
+        {
+            Color hintColor;
+
+            if (currentPlayer == 1)
+            {
+                hintColor = Color.FromArgb(90, 0, 0, 0);
+            }
+            else
+            {
+                hintColor = Color.FromArgb(100, 230, 230, 230);
+            }
+
+            using (SolidBrush hintBrush = new SolidBrush(hintColor))
+            {
+                g.FillEllipse(
+                    hintBrush,
+                    x + size / 4,
+                    y + size / 4,
+                    size / 2,
+                    size / 2
+                );
+            }
+        }
+
+        private void pnlBoard_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (gameOver)
+            {
+                return;
+            }
+
+            if (IsAITurn())
+            {
+                return;
+            }
+
+            cellSize = GetCellSize();
+
+            int col = e.X / cellSize;
+            int row = e.Y / cellSize;
+
+            if (row < 0 || row >= BoardSize || col < 0 || col >= BoardSize)
+            {
+                return;
+            }
+
+            if (e.X >= BoardSize * cellSize || e.Y >= BoardSize * cellSize)
+            {
+                return;
+            }
+
+            if (!IsValidMove(row, col, currentPlayer))
+            {
+                return;
+            }
+
+            PushHistory();
+            PlacePiece(row, col, currentPlayer);
+            NextTurn();
+        }
+
+        private bool IsValidMove(int row, int col, int player)
+        {
+            if (row < 0 || row >= BoardSize || col < 0 || col >= BoardSize)
+            {
+                return false;
+            }
+
+            if (board[row, col] != 0)
+            {
+                return false;
+            }
+
+            int opponent = 3 - player;
+
+            for (int direction = 0; direction < 8; direction++)
+            {
+                int r = row + dr[direction];
+                int c = col + dc[direction];
+
+                bool hasOpponentBetween = false;
+
+                while (IsInsideBoard(r, c) && board[r, c] == opponent)
+                {
+                    hasOpponentBetween = true;
+                    r += dr[direction];
+                    c += dc[direction];
+                }
+
+                if (hasOpponentBetween && IsInsideBoard(r, c) && board[r, c] == player)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsInsideBoard(int row, int col)
+        {
+            return row >= 0 && row < BoardSize && col >= 0 && col < BoardSize;
+        }
+
+        private void PlacePiece(int row, int col, int player)
+        {
+            board[row, col] = player;
+
+            int opponent = 3 - player;
+
+            for (int direction = 0; direction < 8; direction++)
+            {
+                int r = row + dr[direction];
+                int c = col + dc[direction];
+
+                List<Point> piecesToFlip = new List<Point>();
+
+                while (IsInsideBoard(r, c) && board[r, c] == opponent)
+                {
+                    piecesToFlip.Add(new Point(c, r));
+                    r += dr[direction];
+                    c += dc[direction];
+                }
+
+                if (piecesToFlip.Count > 0 && IsInsideBoard(r, c) && board[r, c] == player)
+                {
+                    foreach (Point p in piecesToFlip)
+                    {
+                        board[p.Y, p.X] = player;
+                    }
+                }
+            }
+        }
+
+        private bool HasValidMove(int player)
+        {
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    if (IsValidMove(row, col, player))
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private List<Point> GetValidMoves(int player)
+        {
+            List<Point> moves = new List<Point>();
+
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    if (IsValidMove(row, col, player))
+                    {
+                        moves.Add(new Point(col, row));
+                    }
+                }
+            }
+
+            return moves;
+        }
+
+        private void NextTurn()
+        {
+            int nextPlayer = 3 - currentPlayer;
+
+            if (HasValidMove(nextPlayer))
+            {
+                currentPlayer = nextPlayer;
+            }
+            else if (HasValidMove(currentPlayer))
+            {
+                string skippedPlayer = nextPlayer == 1 ? "黑棋" : "白棋";
+
+                MessageBox.Show(
+                    skippedPlayer + "沒有合法落點，跳過回合！",
+                    "跳過回合",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+            }
+            else
+            {
+                gameOver = true;
+                UpdateUI();
+                pnlBoard.Invalidate();
+                ShowResult();
+                return;
+            }
+
+            UpdateUI();
+            pnlBoard.Invalidate();
+            StartAIIfNeeded();
+        }
+
+        private void CountPieces(out int black, out int white)
+        {
+            black = 0;
+            white = 0;
+
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    if (board[row, col] == 1)
+                    {
+                        black++;
+                    }
+                    else if (board[row, col] == 2)
+                    {
+                        white++;
+                    }
+                }
+            }
+        }
+
+        private void UpdateUI()
+        {
+            int black;
+            int white;
+            CountPieces(out black, out white);
+
+            lblBlackScore.Text = "黑棋: " + black;
+            lblWhiteScore.Text = "白棋: " + white;
+
+            string playerName = currentPlayer == 1 ? "黑棋" : "白棋";
+
+            if (gameOver)
+            {
+                lblStatus.Text = "遊戲結束";
+            }
+            else
+            {
+                lblStatus.Text = "輪到: " + playerName;
+            }
+        }
+
+        private void ShowResult()
+        {
+            int black;
+            int white;
+            CountPieces(out black, out white);
+
+            string result;
+
+            if (black > white)
+            {
+                result = "黑棋獲勝！";
+            }
+            else if (white > black)
+            {
+                result = "白棋獲勝！";
+            }
+            else
+            {
+                result = "平手！";
+            }
+
+            MessageBox.Show(
+                result + "\n\n黑棋: " + black + "\n白棋: " + white,
+                "遊戲結束",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void PushHistory()
+        {
+            int[,] snap = new int[BoardSize, BoardSize];
+            Array.Copy(board, snap, board.Length);
+
+            history.Push(new GameState
+            {
+                BoardSnap = snap,
+                Player = currentPlayer
+            });
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show(
+                "確定要開始新遊戲嗎？",
+                "新遊戲",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                InitGame();
+            }
+        }
+
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            timerAI.Stop();
+
+            if (history.Count == 0)
+            {
+                MessageBox.Show(
+                    "目前沒有可以悔棋的紀錄！",
+                    "悔棋",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information
+                );
+                return;
+            }
+
+            GameState lastState = history.Pop();
+
+            board = lastState.BoardSnap;
+            currentPlayer = lastState.Player;
+            gameOver = false;
+
+            UpdateUI();
+            pnlBoard.Invalidate();
+            StartAIIfNeeded();
+        }
+
+        private bool IsAIMode()
+        {
+            return mode.SelectedIndex == 1 || mode.SelectedIndex == 2;
+        }
+
+        private bool IsAITurn()
+        {
+            return IsAIMode() && currentPlayer == 2;
+        }
+
+        private void StartAIIfNeeded()
+        {
+            timerAI.Stop();
+
+            if (!gameOver && IsAITurn())
+            {
+                timerAI.Start();
+            }
+        }
+
+        private void timerAI_Tick(object sender, EventArgs e)
+        {
+            timerAI.Stop();
+
+            if (gameOver)
+            {
+                return;
+            }
+
+            if (!IsAITurn())
+            {
+                return;
+            }
+
+            List<Point> moves = GetValidMoves(currentPlayer);
+
+            if (moves.Count == 0)
+            {
+                NextTurn();
+                return;
+            }
+
+            Point aiMove = ChooseAIMove(moves);
+
+            PushHistory();
+            PlacePiece(aiMove.Y, aiMove.X, currentPlayer);
+            NextTurn();
+        }
+
+        private Point ChooseAIMove(List<Point> moves)
+        {
+            // 人 vs AI（簡易）：隨機落子
+            if (mode.SelectedIndex == 1)
+            {
+                return moves[random.Next(moves.Count)];
+            }
+
+            // 人 vs AI（稍難）：優先選擇翻最多棋，角落加分
+            Point bestMove = moves[0];
+            int bestScore = int.MinValue;
+
+            foreach (Point move in moves)
+            {
+                int row = move.Y;
+                int col = move.X;
+
+                int score = CountFlips(row, col, currentPlayer) * 10;
+
+                if (IsCorner(row, col))
+                {
+                    score += 1000;
+                }
+                else if (IsEdge(row, col))
+                {
+                    score += 30;
+                }
+
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestMove = move;
+                }
+                else if (score == bestScore && random.Next(2) == 0)
+                {
+                    bestMove = move;
+                }
+            }
+
+            return bestMove;
+        }
+
+        private int CountFlips(int row, int col, int player)
+        {
+            if (!IsValidMove(row, col, player))
+            {
+                return 0;
+            }
+
+            int total = 0;
+            int opponent = 3 - player;
+
+            for (int direction = 0; direction < 8; direction++)
+            {
+                int r = row + dr[direction];
+                int c = col + dc[direction];
+
+                int count = 0;
+
+                while (IsInsideBoard(r, c) && board[r, c] == opponent)
+                {
+                    count++;
+                    r += dr[direction];
+                    c += dc[direction];
+                }
+
+                if (count > 0 && IsInsideBoard(r, c) && board[r, c] == player)
+                {
+                    total += count;
+                }
+            }
+
+            return total;
+        }
+
+        private bool IsCorner(int row, int col)
+        {
+            return
+                (row == 0 && col == 0) ||
+                (row == 0 && col == BoardSize - 1) ||
+                (row == BoardSize - 1 && col == 0) ||
+                (row == BoardSize - 1 && col == BoardSize - 1);
+        }
+
+        private bool IsEdge(int row, int col)
+        {
+            return row == 0 || row == BoardSize - 1 || col == 0 || col == BoardSize - 1;
+        }
+
+        private void mode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            StartAIIfNeeded();
+        }
+
+        private void 遊戲說明ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "【黑白棋 Othello 規則】\n\n" +
+                "1. 棋盤為 8×8，黑棋先手。\n\n" +
+                "2. 落子時，必須在橫、直、斜任一方向夾住對手棋子。\n\n" +
+                "3. 被夾住的對手棋子會全部翻成自己的顏色。\n\n" +
+                "4. 如果輪到某方時沒有合法落點，該方會跳過回合。\n\n" +
+                "5. 雙方都無法落子，或棋盤已滿時，遊戲結束。\n\n" +
+                "6. 棋子數較多者獲勝；數量相同則平手。\n\n" +
+                "7. 盤面上的半透明小圓點代表目前可以落子的位置。\n\n"+ 
+                "8. 若當回合沒有合法的落棋位置，會跳過該回合。",
+                "遊戲規則說明",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+
+        private void 關於ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "黑白棋 Othello v1.0\n製作者：元智大學資工系三年級 Mandy Hsu",
+                "關於",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information
+            );
+        }
+    }
+}
